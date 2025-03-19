c46f5ee16c8d165e834d88cadbdb78a4e465ca2b
ChristianSouza12 <christianlmsouza@outlook.com>
2025-03-19 10:30:18 -0300

    tests: add tests for text fields and radio button

--- cypress/e2e/elements.spec.js
+++ cypress/e2e/elements.spec.js
@@ -2,7 +2,15 @@
 
 
 
+
+
 describe("Work with basic elements", () => {
+
+   beforeEach(() =>{
+    cy.reload()
+   })
+
+
     it("Texts" , () => {
         cy.visit("https://wcaquino.me/cypress/componentes.html")
         cy.get(".facilAchar").should("contain","Cuidado onde clica, muitas armadilhas...")
@@ -10,14 +18,56 @@ describe("Work with basic elements", () => {
     })
 
     it("Links" , () => {
+       
         cy.visit("https://wcaquino.me/cypress/componentes.html")
-        // cy.get("a").first().click()
+        cy.get("#resultado").should("have.not.text", "Voltou!")
+
         cy.contains("Voltar").click()
         cy.get("#resultado").should("have.text", "Voltou!")
 
-        cy.reload()
-        cy.get("#resultado").should("have.not.text", "Voltou!")
+    
 
+    })
+
+    it("TextsFields" , () => {
+        cy.visit("https://wcaquino.me/cypress/componentes.html")
+        
+        cy.get('#formNome').type("Cypress Test")
+        cy.get("#formNome").should("have.value", "Cypress Test")
+
+
+        cy.get("#formSobrenome").type("Cypress teste 123456{backspace}{backspace}")
+        .should("have.value", "Cypress teste 1234")
+
+
+
+        cy.get("#elementosForm\\:sugestoes")
+        .type("Text Area")
+        .should("have.value", "Text Area")
+
+
+
+        cy.get('#tabelaUsuarios input[type="text"]').eq(0)
+        .type("Ola").should("have.value" , "Ola")
+
+
+
+        cy.get("#elementosForm\\:sugestoes")
+        .clear()
+        .type("Erro{selectAll}acerto",{delay:100})
+        .should("have.value", "acerto")
+    })
+
+
+    it("Radio button" , () => {
+        cy.visit("https://wcaquino.me/cypress/componentes.html")
+
+
+        cy.get("#formSexoMasc")
+        .click()
+        .should("be.checked")
+
+        cy.get("#formSexoFem").should("not.be.checked")
     })
 })
 
